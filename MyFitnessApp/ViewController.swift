cfc6c5b274777f54a79dc29404acef2b68ec6a04
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2022-08-23 06:34:50 +0200

    Initial Commit

---
+++ MyFitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFitnessApp
+//
+//  Created by Tomasz Kubiak on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
